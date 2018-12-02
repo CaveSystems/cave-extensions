d356565aeb651f88e55b2b7e09389bf28dafbad2
Dingsd4 <39777727+Dingsd4@users.noreply.github.com>
2018-12-02 15:34:26 +0100

    +code cleanup

--- Cave.Extensions/StreamExtensions.cs
+++ Cave.Extensions/StreamExtensions.cs
@@ -1,54 +1,8 @@
-#region CopyRight 2018
-/*
-    Copyright (c) 2003-2018 Andreas Rohleder (andreas@rohleder.cc)
-    All rights reserved
-*/
-#endregion
-#region License LGPL-3
-/*
-    This program/library/sourcecode is free software; you can redistribute it
-    and/or modify it under the terms of the GNU Lesser General Public License
-    version 3 as published by the Free Software Foundation subsequent called
-    the License.
-
-    You may not use this program/library/sourcecode except in compliance
-    with the License. The License is included in the LICENSE file
-    found at the installation directory or the distribution package.
-
-    Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the
-    "Software"), to deal in the Software without restriction, including
-    without limitation the rights to use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of the Software, and to
-    permit persons to whom the Software is furnished to do so, subject to
-    the following conditions:
-
-    The above copyright notice and this permission notice shall be included
-    in all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-#endregion
-#region Authors & Contributors
-/*
-   Author:
-     Andreas Rohleder <andreas@rohleder.cc>
-
-   Contributors:
- */
-#endregion
-
 using System;
 using System.IO;
 using System.Text;
 
-namespace Cave.IO
+namespace Cave
 {
     /// <summary>
     /// Provides extensions to <see cref="Stream"/> implementations
@@ -155,7 +109,7 @@ namespace Cave.IO
                 while (done < length)
                 {
                     int read = source.Read(buffer, done, (int)length - done);
-                    var e = new ProgressEventArgs(userItem, done, read, length, true);
+                    ProgressEventArgs e = new ProgressEventArgs(userItem, done, read, length, true);
                     callback?.Invoke(source, e);
                     if (read == -1 || e.Break)
                     {
