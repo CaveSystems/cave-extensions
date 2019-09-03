98228d31312eff0e7587aba6c4b73e655294d66a
Andreas Rohleder <andreas@rohleder.cc>
2019-09-03 21:48:05 +0200

    Added bitwise Rol, Ror

---
+++ Cave.Extensions/BitwiseExtensions.cs
@@ -0,0 +1,160 @@
+﻿namespace Cave
+{
+    /// <summary>
+    /// Provides extensions working on the bits of system types.
+    /// </summary>
+    public static class BitwiseExtensions
+    {
+        #region int
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Rol(ref this int value, int count) => value = (int)((((uint)value) << count) | (((uint)value) >> (32 - count)));
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Rol(ref this int value) => value = (int)((((uint)value) << 1) | (((uint)value) >> 31));
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Ror(ref this int value, int count) => value = (int)((((uint)value) >> count) | (((uint)value) << (32 - count)));
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Ror(ref this int value) => value = (int)((((uint)value) >> 1) | (((uint)value) << 31));
+
+        #endregion
+
+        #region uint
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Rol(ref this uint value, int count) => value = (value << count) | (value >> (32 - count));
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Rol(ref this uint value) => value = (value << 1) | (value >> 31);
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Ror(ref this uint value, int count) => value = (value >> count) | (value << (32 - count));
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Ror(ref this uint value) => value = (value >> 1) | (value << 31);
+
+        #endregion
+
+        #region long
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Rol(ref this long value, int count) => value = (long)((((ulong)value) << count) | (((ulong)value) >> (64 - count)));
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Rol(ref this long value) => value = (long)((((ulong)value) << 1) | (((ulong)value) >> 63));
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Ror(ref this long value, int count) => value = (long)((((ulong)value) >> count) | (((ulong)value) << (64 - count)));
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Ror(ref this long value) => value = (long)((((ulong)value) >> 1) | (((ulong)value) << 63));
+
+        #endregion
+
+        #region ulong
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Rol(ref this ulong value, int count) => value = (value << count) | (value >> (64 - count));
+
+        /// <summary>
+        /// ROL rotates the bits within the destination operand to the left, where left is toward the most significant bit (MSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the leftmost bit of the operand is shifted into the rightmost bit,
+        /// and all intermediate bits are shifted one bit to the left. Except for the direction the shift operation takes, ROL is identical to ROR.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Rol(ref this ulong value) => value = (value << 1) | (value >> 63);
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="count">The number of bits to rotete.</param>
+        public static void Ror(ref this ulong value, int count) => value = (value >> count) | (value << (64 - count));
+
+        /// <summary>
+        /// ROR rotates the bits within the destination operand to the right, where right is toward the least significant bit (LSB).
+        /// A rotate is a shift (see SHL and SHR) that wraps around; the rightmost bit of the operand is shifted into the leftmost bit,
+        /// and all intermediate bits are shifted one bit to the right. Except for the direction the shift operation takes, ROR is identical to ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        public static void Ror(ref this ulong value) => value = (value >> 1) | (value << 63);
+
+        #endregion
+    }
+}
