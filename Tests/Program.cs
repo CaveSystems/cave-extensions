a53b67f9744aa64c4d6149dd1911ef2f1ed7a011
Andreas Rohleder <andreas.rohleder@daimler.com>
2019-11-24 18:55:38 +0100

    Added tests for double and float ToString() and ConvertValue() extensions.

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -19,22 +19,21 @@ namespace Test
                 }
 
                 var instance = Activator.CreateInstance(type);
-                foreach (System.Reflection.MethodInfo method in type.GetMethods())
+                foreach (var method in type.GetMethods())
                 {
                     if (!method.GetCustomAttributes(typeof(TestAttribute), false).Any())
                     {
                         continue;
                     }
 
-                    GC.Collect(999, GCCollectionMode.Default, true);
+                    GC.Collect(999, GCCollectionMode.Default);
 
                     Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.WriteLine($"{method.DeclaringType.Name}.cs: info TI0001: Start {method.Name}");
                     Console.ResetColor();
                     try
                     {
-                        var action = (Action)method.CreateDelegate(typeof(Action), instance);
-                        action();
+                        method.Invoke(instance, null);
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine($"{method.DeclaringType.Name}.cs: info TI0002: Success {method.Name}");
                         Console.ResetColor();
--- Tests/StringExtensionTests.cs
+++ Tests/StringExtensionTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Text;
 using Cave;
 using NUnit.Framework;
@@ -20,6 +21,27 @@ namespace Tests
             var escaped = StringExtensions.Escape(text);
             var unescaped = StringExtensions.Unescape(escaped);
             Assert.AreEqual(text, unescaped);
+        }
+
+        [Test]
+        public void ToStringParse()
+        {
+            Random rnd = new Random();
+            var allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (var culture in allCultures)
+            {
+                float f = (float)rnd.NextDouble();
+                var s = StringExtensions.ToString(f, culture);
+                var test = TypeExtension.ConvertValue(typeof(float), s, culture);
+                Assert.AreEqual(f, test);
+            }
+            foreach (var culture in allCultures)
+            {
+                double d = rnd.NextDouble();
+                var s = StringExtensions.ToString(d, culture);
+                var test = TypeExtension.ConvertValue(typeof(double), s, culture);
+                Assert.AreEqual(d, test);
+            }
         }
     }
 }
