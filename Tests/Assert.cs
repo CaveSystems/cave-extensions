e1d66d27717bc1f73be03fecb93fa25f8b127281
Andreas Rohleder <andreas@rohleder.cc>
2020-07-09 21:23:24 +0200

    Feature: Assert.AreEqual replacement for NET2.0 and 3.5 now supports sequences.

--- Tests/Assert.cs
+++ Tests/Assert.cs
@@ -1,6 +1,8 @@
 ﻿#if NET20 || NET35
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace NUnit.Framework
 {
@@ -8,6 +10,46 @@ namespace NUnit.Framework
     {
         public static void AreEqual<T>(T expected, T actual)
         {
+            if (!(expected is string))
+            {
+                if (expected is IEnumerable ie1)
+                {
+                    if (actual is IEnumerable ie2)
+                    {
+                        var e1 = ie1.GetEnumerator();
+                        var e2 = ie2.GetEnumerator();
+                        e1.Reset();
+                        e2.Reset();
+
+                        for (int i = 0; ; i++)
+                        {
+                            if (e1.MoveNext())
+                            {
+                                if (e2.MoveNext())
+                                {
+                                    if (!object.Equals(e1.Current, e2.Current))
+                                    {
+                                        throw new Exception("Items are not equal!");
+                                    }
+                                }
+                                else
+                                {
+                                    throw new Exception("Items are not equal!");
+                                }
+                            }
+                            else
+                            {
+                                if (e2.MoveNext())
+                                {
+                                    throw new Exception("Items are not equal!");
+                                }
+                                return;
+                            }
+                        }
+                    }
+                    throw new Exception("Items are not equal!");
+                }
+            }
             if (!object.Equals(expected, actual))
             {
                 throw new Exception("Items are not equal!");
