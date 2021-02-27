a55542820d5925b25c05b289598585fe6059be4d
Andreas Rohleder <andreas@rohleder.cc>
2021-02-27 16:18:32 +0100

    Added tests for Lazy backport

---
+++ Tests/LazyTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    class LazyTests
+    {
+        #region Public Methods
+
+        [Test]
+        public void Lazy()
+        {
+            var test = new Lazy<int>(() => 1);
+            Assert.IsFalse(test.IsValueCreated);
+            Assert.AreEqual(test.Value, 1);
+            Assert.IsTrue(test.IsValueCreated);
+        }
+
+        [Test]
+        public void LazyEx()
+        {
+            var test = new Lazy<int>(() => throw new TimeoutException());
+            Assert.IsFalse(test.IsValueCreated);
+            void GetValue() { if (test.Value != 0) { throw new Exception(); } }
+            Assert.Throws<TimeoutException>(GetValue);
+            Assert.IsFalse(test.IsValueCreated);
+        }
+
+        #endregion Public Methods
+    }
+}
