1564915f91b765b5b5225a06589abe9b45479f9a
Andreas Rohleder <andreas@rohleder.cc>
2021-02-27 14:33:33 +0100

    Added Lazy backport.

--- Cave.Extensions/IPNetwork.cs
+++ Cave.Extensions/IPNetwork.cs
@@ -4,20 +4,26 @@ using System.Net.Sockets;
 
 namespace Cave
 {
-    /// <summary>Gets an ipv4 subnet definition.</summary>
+    /// <summary>
+    /// Gets an ipv4 subnet definition.
+    /// </summary>
     public class IPNetwork : IEquatable<IPNetwork>
     {
+        #region Private Methods
+
         static IPAddress GetMask(int subnet, AddressFamily addressFamily)
         {
             byte[] result;
             switch (addressFamily)
             {
                 case AddressFamily.InterNetwork:
-                    result = new byte[4];
-                    break;
+                result = new byte[4];
+                break;
+
                 case AddressFamily.InterNetworkV6:
-                    result = new byte[16];
-                    break;
+                result = new byte[16];
+                break;
+
                 default: throw new ArgumentOutOfRangeException(nameof(addressFamily));
             }
             var byteCount = subnet / 8;
@@ -33,14 +39,22 @@ namespace Cave
             return new IPAddress(result);
         }
 
-        /// <summary>Initializes a new instance of the <see cref="IPNetwork" /> class.</summary>
+        #endregion Private Methods
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPNetwork"/> class.
+        /// </summary>
         /// <param name="address">Base address.</param>
         /// <param name="subnet">Subnet.</param>
         public IPNetwork(IPAddress address, int subnet) : this(address, GetMask(subnet, address?.AddressFamily ?? throw new ArgumentNullException(nameof(address))))
         {
         }
 
-        /// <summary>Initializes a new instance of the <see cref="IPNetwork" /> class.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPNetwork"/> class.
+        /// </summary>
         /// <param name="address">Base address.</param>
         /// <param name="mask">Subnet mask.</param>
         public IPNetwork(IPAddress address, IPAddress mask)
@@ -52,7 +66,7 @@ namespace Cave
             {
                 throw new ArgumentException("Address length does not match!");
             }
-            
+
             var bitCounter = 0;
             var bitsMixed = false;
             var bitsDone = false;
@@ -113,26 +127,44 @@ namespace Cave
             Broadcast = new IPAddress(broadcastBytes);
         }
 
-        /// <summary>Gets the name of the reverse lookup zone.</summary>
-        public string ReverseLookupZone => Subnet > 0 ? Address.GetReverseLookupZone(Subnet) : Address.GetReverseLookupZone(Mask);
+        #endregion Public Constructors
 
-        /// <summary>Gets the base ip address.</summary>
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the base ip address.
+        /// </summary>
         public IPAddress Address { get; }
-
-        /// <summary>Gets the subnet mask.</summary>
-        public IPAddress Mask { get; }
-
-        /// <summary>Gets the subnet or -1 if the mask has to be used.</summary>
-        public int Subnet { get; }
 
         /// <summary>
         /// Gets the broadcast address.
         /// </summary>
         public IPAddress Broadcast { get; }
 
-        /// <summary>Parses an <see cref="IPNetwork" /> instance from the specified string.</summary>
+        /// <summary>
+        /// Gets the subnet mask.
+        /// </summary>
+        public IPAddress Mask { get; }
+
+        /// <summary>
+        /// Gets the name of the reverse lookup zone.
+        /// </summary>
+        public string ReverseLookupZone => Subnet > 0 ? Address.GetReverseLookupZone(Subnet) : Address.GetReverseLookupZone(Mask);
+
+        /// <summary>
+        /// Gets the subnet or -1 if the mask has to be used.
+        /// </summary>
+        public int Subnet { get; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        /// <summary>
+        /// Parses an <see cref="IPNetwork"/> instance from the specified string.
+        /// </summary>
         /// <param name="text">String to parse.</param>
-        /// <returns>Returns a new <see cref="IPNetwork" /> instance.</returns>
+        /// <returns>Returns a new <see cref="IPNetwork"/> instance.</returns>
         public static IPNetwork Parse(string text)
         {
             if (text == null) throw new ArgumentNullException(nameof(text));
@@ -158,8 +190,15 @@ namespace Cave
         /// <inheritdoc/>
         public override bool Equals(object obj) => Equals(obj as IPNetwork);
 
-        /// <summary>Gets a string {ipaddress}/{subnet} or {ipaddress}/{mask}. This can be parsed by <see cref="Parse(string)" />.</summary>
+        /// <inheritdoc/>
+        public override int GetHashCode() => ToString().GetHashCode();
+
+        /// <summary>
+        /// Gets a string {ipaddress}/{subnet} or {ipaddress}/{mask}. This can be parsed by <see cref="Parse(string)"/>.
+        /// </summary>
         /// <returns>Parsable string describing this instance.</returns>
         public override string ToString() => $"{Address}/{(Subnet > -1 ? Subnet : (object)Mask)}";
+
+        #endregion Public Methods
     }
 }
