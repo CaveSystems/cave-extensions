c23f2dfa25f5f8f8eb695ad5b90d4e688da53858
Andreas Rohleder <andreas@rohleder.cc>
2021-07-23 10:05:40 +0200

    Added SemanticVersion test cases.

--- Tests/SemanticVersionTests.cs
+++ Tests/SemanticVersionTests.cs
@@ -11,12 +11,14 @@ namespace Test
         {
             var ver = SemanticVersion.Parse("1.2-test");
             Assert.AreEqual("1.2-test", ver.ToString());
-            Assert.AreEqual("1.2", ver.GetClassicVersion().ToString());
+            Assert.AreEqual("1.2", ver.GetNormalizedVersion().ToString());
             Assert.AreEqual(1, ver.Major);
             Assert.AreEqual(2, ver.Minor);
             Assert.AreEqual(-1, ver.Patch);
             Assert.AreEqual("-test", ver.Meta);
             Assert.AreEqual(true, ver.IsMetaValid);
+            Assert.IsTrue(ver.GetNormalizedVersion() < ver);
+            Assert.IsTrue(ver.GetNormalizedVersion() <= ver);
         }
 
         [Test]
@@ -24,12 +26,15 @@ namespace Test
         {
             var ver = SemanticVersion.Parse("1.2.3");
             Assert.AreEqual("1.2.3", ver.ToString());
-            Assert.AreEqual("1.2.3", ver.GetClassicVersion().ToString());
+            Assert.AreEqual("1.2.3", ver.GetNormalizedVersion().ToString());
             Assert.AreEqual(1, ver.Major);
             Assert.AreEqual(2, ver.Minor);
             Assert.AreEqual(3, ver.Patch);
             Assert.AreEqual(null, ver.Meta);
             Assert.AreEqual(true, ver.IsMetaValid);
+            Assert.AreEqual((SemanticVersion)ver.GetNormalizedVersion(), ver);
+            Assert.IsTrue(ver.GetNormalizedVersion() == ver);
+            Assert.IsTrue(ver.GetNormalizedVersion() <= ver);
         }
 
         [Test]
@@ -37,12 +42,14 @@ namespace Test
         {
             var ver = SemanticVersion.Parse("1.2.3.4.5.6.7.8-test");
             Assert.AreEqual("1.2.3.4.5.6.7.8-test", ver.ToString());
-            Assert.AreEqual("1.2.3", ver.GetClassicVersion().ToString());
+            Assert.AreEqual("1.2.3", ver.GetNormalizedVersion().ToString());
             Assert.AreEqual(1, ver.Major);
             Assert.AreEqual(2, ver.Minor);
             Assert.AreEqual(3, ver.Patch);
             Assert.AreEqual(".4.5.6.7.8-test", ver.Meta);
             Assert.AreEqual(false, ver.IsMetaValid);
+            Assert.IsTrue(ver.GetNormalizedVersion() < ver);
+            Assert.IsTrue(ver.GetNormalizedVersion() <= ver);
         }
 
         [Test]
@@ -50,12 +57,14 @@ namespace Test
         {
             var ver = SemanticVersion.Parse("2015.1234.56789-metadata-v1.2");
             Assert.AreEqual("2015.1234.56789-metadata-v1.2", ver.ToString());
-            Assert.AreEqual("2015.1234.56789", ver.GetClassicVersion().ToString());
+            Assert.AreEqual("2015.1234.56789", ver.GetNormalizedVersion().ToString());
             Assert.AreEqual(2015, ver.Major);
             Assert.AreEqual(1234, ver.Minor);
             Assert.AreEqual(56789, ver.Patch);
             Assert.AreEqual("-metadata-v1.2", ver.Meta);
             Assert.AreEqual(true, ver.IsMetaValid);
+            Assert.IsTrue(ver.GetNormalizedVersion() < ver);
+            Assert.IsTrue(ver.GetNormalizedVersion() <= ver);
         }
 
         [Test]
@@ -63,12 +72,14 @@ namespace Test
         {
             var ver = SemanticVersion.Parse("2015.1234.56789+metadata-test-123+678");
             Assert.AreEqual("2015.1234.56789+metadata-test-123+678", ver.ToString());
-            Assert.AreEqual("2015.1234.56789", ver.GetClassicVersion().ToString());
+            Assert.AreEqual("2015.1234.56789", ver.GetNormalizedVersion().ToString());
             Assert.AreEqual(2015, ver.Major);
             Assert.AreEqual(1234, ver.Minor);
             Assert.AreEqual(56789, ver.Patch);
             Assert.AreEqual("+metadata-test-123+678", ver.Meta);
             Assert.AreEqual(true, ver.IsMetaValid);
+            Assert.IsTrue(ver.GetNormalizedVersion() < ver);
+            Assert.IsTrue(ver.GetNormalizedVersion() <= ver);
         }
 
         [Test]
@@ -76,12 +87,32 @@ namespace Test
         {
             var ver = SemanticVersion.TryParse("2015.1234.56789+metadata-test-123 INVALID!");
             Assert.AreEqual("2015.1234.56789+metadata-test-123 INVALID!", ver.ToString());
-            Assert.AreEqual("2015.1234.56789", ver.GetClassicVersion().ToString());
+            Assert.AreEqual("2015.1234.56789", ver.GetNormalizedVersion().ToString());
             Assert.AreEqual(2015, ver.Major);
             Assert.AreEqual(1234, ver.Minor);
             Assert.AreEqual(56789, ver.Patch);
             Assert.AreEqual("+metadata-test-123 INVALID!", ver.Meta);
             Assert.AreEqual(false, ver.IsMetaValid);
+            Assert.IsTrue(ver.GetNormalizedVersion() < ver);
+            Assert.IsTrue(ver.GetNormalizedVersion() <= ver);
+        }
+
+        [Test]
+        public void EqualTest()
+        {
+            var version1 = SemanticVersion.Parse("1.2");
+            var version2 = SemanticVersion.Parse("1.2.0");
+            Assert.IsTrue(version1 == version2);
+            Assert.IsTrue(Equals(version1, version2));
+            Assert.IsFalse(version1 < version2);
+            Assert.IsFalse(version1 > version2);
+
+            var version3 = SemanticVersion.Parse("1.2-meta");
+            var version4 = SemanticVersion.Parse("1.2.0-meta");
+            Assert.IsTrue(version3 == version4);
+            Assert.IsTrue(Equals(version3, version4));
+            Assert.IsFalse(version3 < version4);
+            Assert.IsFalse(version3 > version4);
         }
 
         [Test]
