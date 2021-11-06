8c34aff0663fb2c9d103569ee9a2d2ee5727a420
Andreas Rohleder <andreas@rohleder.cc>
2021-11-06 16:00:46 +0100

    Added testcase for AppDom.FindType()

---
+++ Tests/AppDomTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Cave;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    class AppDomTests
+    {
+        [Test]
+        public void SystemUri()
+        {
+            var type = typeof(Uri);
+            var result = AppDom.FindType(type.AssemblyQualifiedName);
+            Assert.AreEqual(type, result);
+        }
+    }
+}
