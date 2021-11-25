2f193e6c04610d7f78189155bb522a79a65d6b34
Andreas Rohleder <andreas@rohleder.cc>
2021-11-25 19:59:58 +0100

    Extensions for IPNetwork: + Addresses enumeration + new property AddressesCount + new property AddressesBitCount

--- Cave.Extensions/IPNetwork.cs
+++ Cave.Extensions/IPNetwork.cs
@@ -1,15 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 
 namespace Cave
 {
-    /// <summary>Gets an ipv4 subnet definition.</summary>
+    /// <summary>
+    /// Gets an ipv4 subnet definition.
+    /// </summary>
     public class IPNetwork : IEquatable<IPNetwork>
     {
         #region Static
 
         #region Private Methods
+
+        static IEnumerable<byte> GetBits(byte b)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                yield return (byte)((b >> i) & 1);
+            }
+        }
+
+        static IEnumerable<byte> GetBytes(IEnumerable<byte> bits)
+        {
+            var i = 0;
+            byte current = 0;
+            foreach (var bit in bits)
+            {
+                current |= (byte)(bit << i);
+                if (++i == 8)
+                {
+                    i = 0;
+                    yield return current;
+                    current = 0;
+                }
+            }
+        }
 
         static IPAddress GetMask(int subnet, AddressFamily addressFamily)
         {
@@ -36,18 +64,22 @@ namespace Cave
 
         #endregion Private Methods
 
-        #endregion
+        #endregion Static
 
         #region Constructors
 
-        /// <summary>Initializes a new instance of the <see cref="IPNetwork" /> class.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPNetwork"/> class.
+        /// </summary>
         /// <param name="address">Base address.</param>
         /// <param name="subnet">Subnet.</param>
         public IPNetwork(IPAddress address, int subnet) : this(address, GetMask(subnet, address?.AddressFamily ?? throw new ArgumentNullException(nameof(address))))
         {
         }
 
-        /// <summary>Initializes a new instance of the <see cref="IPNetwork" /> class.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPNetwork"/> class.
+        /// </summary>
         /// <param name="address">Base address.</param>
         /// <param name="mask">Subnet mask.</param>
         public IPNetwork(IPAddress address, IPAddress mask)
@@ -123,32 +155,110 @@ namespace Cave
             Broadcast = new IPAddress(broadcastBytes);
         }
 
-        #endregion
+        #endregion Constructors
 
         #region Properties
 
-        /// <summary>Gets the base ip address.</summary>
+        /// <summary>
+        /// Gets the base ip address.
+        /// </summary>
         public IPAddress Address { get; }
 
-        /// <summary>Gets the broadcast address.</summary>
+        /// <summary>
+        /// Gets the broadcast address.
+        /// </summary>
         public IPAddress Broadcast { get; }
 
-        /// <summary>Gets the subnet mask.</summary>
+        /// <summary>
+        /// Gets the subnet mask.
+        /// </summary>
         public IPAddress Mask { get; }
 
-        /// <summary>Gets the name of the reverse lookup zone.</summary>
+        /// <summary>
+        /// Gets the name of the reverse lookup zone.
+        /// </summary>
         public string ReverseLookupZone => Subnet > 0 ? Address.GetReverseLookupZone(Subnet) : Address.GetReverseLookupZone(Mask);
 
-        /// <summary>Gets the subnet or -1 if the mask has to be used.</summary>
+        /// <summary>
+        /// Gets the subnet or -1 if the mask has to be used.
+        /// </summary>
         public int Subnet { get; }
 
-        #endregion
+        /// <summary>
+        /// Gets an enumerator for all addresses present at the network.
+        /// </summary>
+        public IEnumerable<IPAddress> Addresses
+        {
+            get
+            {
+                if (AddressesBitCount == 0)
+                {
+                    yield return Address;
+                }
+                else
+                {
+                    var mask = Mask.GetAddressBytes().Reverse().SelectMany(GetBits).ToArray();
+                    var value = Address.GetAddressBytes().Reverse().SelectMany(GetBits).ToArray();
+#if DEBUG
+                    var test = new IPAddress(GetBytes(value).Reverse().ToArray());
+                    if (!Equals(Address, test)) throw new Exception("Adress roundtrip test failed!");
+#endif
+                    while (true)
+                    {
+                        var done = false;
+                        for (var n = 0; n < mask.Length; n++)
+                        {
+                            if (mask[n] == 1)
+                            {
+                                continue;
+                            }
+                            if (value[n] == 0)
+                            {
+                                value[n] = 1;
+                                done = true;
+                                break;
+                            }
+                            value[n] = 0;
+                        }
+                        if (!done) break;
+
+                        var address = GetBytes(value).Reverse().ToArray();
+                        yield return new IPAddress(address);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of addresses present at the network. This returns 0 for networks with subnets greater than 64 bits.
+        /// </summary>
+        /// <remarks>This is a fast calculation for all addresses present at <see cref="Addresses"/>.</remarks>
+        public ulong AddressesCount
+        {
+            get
+            {
+                var bits = AddressesBitCount;
+                if (bits == 0) return 1;
+                if (bits == 64) return ulong.MaxValue;
+                if (bits > 64) return 0;
+                return (1UL << bits) - 1;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of bits used for addresses at the network.
+        /// </summary>
+        public int AddressesBitCount => Mask.GetAddressBytes().SelectMany(GetBits).Count(b => b == 0);
+
+        #endregion Properties
 
         #region Public Methods
 
-        /// <summary>Parses an <see cref="IPNetwork" /> instance from the specified string.</summary>
+        /// <summary>
+        /// Parses an <see cref="IPNetwork"/> instance from the specified string.
+        /// </summary>
         /// <param name="text">String to parse.</param>
-        /// <returns>Returns a new <see cref="IPNetwork" /> instance.</returns>
+        /// <returns>Returns a new <see cref="IPNetwork"/> instance.</returns>
         public static IPNetwork Parse(string text)
         {
             if (text == null)
@@ -176,16 +286,18 @@ namespace Cave
             return new IPNetwork(addr, mask);
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public bool Equals(IPNetwork other) => (other != null) && Equals(other.Address, Address) && Equals(other.Mask, Mask) && (other.Subnet == Subnet);
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public override bool Equals(object obj) => Equals(obj as IPNetwork);
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public override int GetHashCode() => ToString().GetHashCode();
 
-        /// <summary>Gets a string {ipaddress}/{subnet} or {ipaddress}/{mask}. This can be parsed by <see cref="Parse(string)" />.</summary>
+        /// <summary>
+        /// Gets a string {ipaddress}/{subnet} or {ipaddress}/{mask}. This can be parsed by <see cref="Parse(string)"/>.
+        /// </summary>
         /// <returns>Parsable string describing this instance.</returns>
         public override string ToString() => $"{Address}/{(Subnet > -1 ? Subnet : (object)Mask)}";
 
