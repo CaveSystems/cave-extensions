efb67b414a6da9b6f59f7af6eef3c06a45317b5d
Andreas Rohleder <andreas@rohleder.cc>
2022-02-05 14:54:02 +0100

    Added IEnumerable .ToArrayList() and .ToObjectArray() extensions

---
+++ Cave.Extensions/Collections/IEnumerableExtension.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+
+namespace Cave.Collections
+{
+    /// <summary>Gets extensions to the IEnumerable interface.</summary>
+    public static class IEnumerableExtension
+    {
+        #region Static
+
+        /// <summary>Converts to a list.</summary>
+        /// <param name="enumerable">The <see cref="IEnumerable"/> instance to convert.</param>
+        /// <returns>Returns a new <see cref="ArrayList"/> instance.</returns>
+        public static ArrayList ToArrayList(this IEnumerable enumerable)
+        {
+            var result = new ArrayList();
+            foreach (var item in enumerable)
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+
+        /// <summary>Converts to an array of <see cref="object"/>.</summary>
+        /// <param name="enumerable">The <see cref="IEnumerable"/> instance to convert.</param>
+        /// <returns>Returns a new array of <see cref="object"/></returns>
+        public static object[] ToObjectArray(this IEnumerable enumerable) => ToArrayList(enumerable).ToArray();
+
+        #endregion
+    }
+}
