bbb6435f34d72a29c41a102841e13a09b0b71794
Andreas Rohleder <andreas@rohleder.cc>
2022-05-08 18:52:03 +0200

    Added string.Contains(char) extension (backport for < net 5.0)

--- Cave.Extensions/StringExtensions.cs
+++ Cave.Extensions/StringExtensions.cs
@@ -163,6 +163,17 @@ namespace Cave
         [MethodImpl(256)]
         public static string Box(this string text, string start, string end) => start + text + end;
 
+#if NET5_0_OR_GREATER
+#else
+        /// <summary>Returns a value indicating whether a specified character occurs within this string.</summary>
+        /// <param name="text">The text.</param>
+        /// <param name="c">The character to seek.</param>
+        /// <remarks>This method performs an ordinal (case-sensitive and culture-insensitive) comparison.</remarks>
+        /// <returns>true if the value parameter occurs within this string; otherwise, false.</returns>
+        [MethodImpl(256)]
+        public static bool Contains(this string text, char c) => text.IndexOf(c) >= 0;
+#endif
+
         /// <summary>Tries to detect the used newline chars in the specified string.</summary>
         /// <param name="text">The text.</param>
         /// <returns>Retruns the detected new line string (CR, LF, CRLF).</returns>
